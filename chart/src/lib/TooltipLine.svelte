cdf9c537176541fbc177ffcf22b3a537722a191d
Elouan-Debels <elouandebels@protonmail.com>
2024-05-05 20:03:41 +0200

    added chart
    added chart

---
+++ chart/src/lib/TooltipLine.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let vline;
+    </script>
+    
+    
+    <g class="tooltip-lines" transform="translate(0, 0)">
+        <!-- vertical line -->
+        <line
+          y1={vline.y1}
+          y2={vline.y2}
+          x1={vline.x1}
+          x2={vline.x2}
+        />
+    </g>
+    
+    <style>
+        .tooltip-lines line {
+        stroke: #f1eeee;
+        stroke-width: 2px;
+        stroke-dasharray: 1.5px;
+      }
+    </style>
